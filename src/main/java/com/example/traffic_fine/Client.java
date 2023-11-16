92d4826ede7c195d375e5714774a20fa6359ff4b
Ekanto3321 <araddhaekanto@gmail.com>
2023-11-16 10:14:52 +0600

    Client Server communication is working

--- src/main/java/com/example/traffic_fine/Client.java
+++ src/main/java/com/example/traffic_fine/Client.java
@@ -2,15 +2,14 @@ package com.example.traffic_fine;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Client{
 
 
     Socket socket = null;
-    InputStreamReader isr = null;
-    OutputStreamWriter osw = null;
+    InputStreamReader in = null;
+    OutputStreamWriter out = null;
     BufferedReader br = null;
     BufferedWriter bw = null;
     String data = "0";
@@ -19,28 +18,46 @@ public class Client{
 
         try {
             socket = new Socket("localhost", 1234);
-            osw = new OutputStreamWriter(socket.getOutputStream());
-            bw = new BufferedWriter(osw);
-            isr = new InputStreamReader(socket.getInputStream());
-            br = new BufferedReader(isr);
-            String receivedCoordinates;
+            out = new OutputStreamWriter(socket.getOutputStream());
+            bw = new BufferedWriter(out);
+            in = new InputStreamReader(socket.getInputStream());
+            br = new BufferedReader(in);
+            String dataToSend;
             Scanner sc = new Scanner(System.in);
             while (true) {
+                dataToSend = sc.nextLine();
 
-                receivedCoordinates = sc.nextLine();
-                bw.write(receivedCoordinates);
+                bw.write(dataToSend);
                 bw.newLine();
                 bw.flush();
-
+                if(dataToSend.equals("exit")){
+                    System.out.println("connection closed");
+                    break;
+                }
                 System.out.println(br.readLine());
+
 
             }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+            try {
+                bw.close();
+                br.close();
+            } catch (IOException e) {
+                closeConnection(socket,br,bw);
+            }
+    }
 
-
+    public void closeConnection(Socket socket, BufferedReader br, BufferedWriter bw){
+        try {
+            if(socket!=null)bw.close();
+            if(socket!=null)socket.close();
+            if(socket!=null)bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String args[]) {
--- src/main/java/com/example/traffic_fine/Server.java
+++ src/main/java/com/example/traffic_fine/Server.java
@@ -7,23 +7,28 @@ import java.util.ArrayList;
 
 public class Server {
 
-    ArrayList<ClientHandler> clients = new ArrayList<>();
+    ArrayList<ClientHandlerAPI> clients = new ArrayList<>();
 
     ServerSocket serverSocket = null;
     Socket socket = null;
 
 
     Server(){
+        try {
+            serverSocket = new ServerSocket(1234);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         while(true) {
             try {
-                serverSocket = new ServerSocket(1234);
                 socket = serverSocket.accept();
 
-                clients.add(new ClientHandler(socket));
+                clients.add(new ClientHandlerAPI(socket));
 
                 System.out.println("A client has connected");
-                Thread thread = new Thread(new ClientHandler(socket));
-                thread.run();
+                ClientHandlerAPI clientHandlerAPI = new ClientHandlerAPI(socket);
+                Thread thread = new Thread(clientHandlerAPI);
+                thread.start();
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -38,7 +43,7 @@ public class Server {
 
 }
 
-class ClientHandler implements Runnable{
+class ClientHandlerAPI implements Runnable{
     InputStreamReader in = null;
     OutputStreamWriter out = null;
     BufferedWriter bw = null;
@@ -46,7 +51,7 @@ class ClientHandler implements Runnable{
     String query;
     String sentData;
     Socket socket;
-    ClientHandler(Socket socket){
+    ClientHandlerAPI(Socket socket){
         this.socket = socket;
     }
 
@@ -60,19 +65,35 @@ class ClientHandler implements Runnable{
         }
         br = new BufferedReader(in);
         bw = new BufferedWriter(out);
-        while (true){
-            try {
-                //complete the code here
-                query = br.readLine();
-                sentData=query;
-                bw.write("From server: "+sentData);
-                bw.newLine();
-                bw.flush();
 
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        try {
+            while (true) {
+                //complete the code here
+                    query = br.readLine();
+                if(query.equals("exit")){
+                    System.out.println("client has closed connection");
+                    break;
+                }
+                    sentData = query;
+                    bw.write("From server: " + sentData);
+                    bw.newLine();
+                    bw.flush();
+
             }
+        } catch (IOException e) {
+                closeConnection(socket,br,bw);
         }
 
     }
+    public void closeConnection(Socket socket, BufferedReader br, BufferedWriter bw){
+        try {
+            if(socket!=null)bw.close();
+            if(socket!=null)socket.close();
+            if(socket!=null)bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
